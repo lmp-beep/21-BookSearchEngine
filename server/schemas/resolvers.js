40c10ac83fd9fd4bb28b586f49c244fe6cb52cc8
Lisa Pessin <lisapessin@gmail.com>
2021-07-10 09:20:36 -0400

    finished resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -18,14 +18,59 @@ const resolvers = {
     },
 
     Mutation: {
-        login:
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
 
-        addUser:
+            if(!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
 
-        saveBook:
+            const correctPw = await user.isCorrectPassword(password);
 
-        removeBook:
+            if(!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+    
+        },
+
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+          
+            return { token, user };
+        },
+
+        saveBook: async (parent, args, context) => {
+            if (context.user) {
+             const updatedUser =  await User.findByIdAndUpdate(
+                { _id: context.user._id },
+                { $addToSet: { savedBooks: args.input } },
+                { new: true }
+              );
+          
+            return updatedUser;
+            }
+          
+            throw new AuthenticationError('You need to be logged in!');
+        },
+
+        removeBook: async (parent, args, context) => {
+            if(context.user) {
+            const updatedUser = await User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $pull: { savedBooks: { bookId: args.bookId } } },
+                { new: true }
+            );
+
+            return updatedUser;
+            }
+
+            throw new AuthenticationError('You need to be logged in!');
+        }
     }
-}
+};
 
 module.exports = resolvers;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+// const routes = require('./routes');
 
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
@@ -30,7 +30,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-app.use(routes);
+// app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => {
